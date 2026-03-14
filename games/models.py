74a9a24c2ae2bdc9142df55476a8d332896bdfdd
Zayed-Alhashmi <123957329+Zayed-Alhashmi@users.noreply.github.com>
2026-03-14 06:30:44 +0000

    Add Game and Review models

--- games/models.py
+++ games/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Game(models.Model):
+    title = models.CharField(max_length=200)
+    genre = models.CharField(max_length=100)
+    platform = models.CharField(max_length=100)
+    release_year = models.IntegerField(null=True, blank=True)
+    developer = models.CharField(max_length=200, null=True, blank=True)
+    critic_score = models.FloatField(null=True, blank=True)
+    age_rating = models.CharField(max_length=10, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+class Review(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name='reviews')
+    reviewer_name = models.CharField(max_length=100)
+    rating = models.IntegerField()
+    comment = models.TextField()
+
+    def __str__(self):
+        return f"{self.reviewer_name} - {self.game.title}"
