d556679a3eeaae3c10c8509fb1c9609f2a1625be
Zayed-Alhashmi <123957329+Zayed-Alhashmi@users.noreply.github.com>
2026-03-15 20:19:02 +0000

    Added Frontend for better navigation and UI and Usability
    -Just access the base URL instead of playing around with the urls
    -QoL in general

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -16,6 +17,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='frontend.html'), name='frontend'),
     path('admin/', admin.site.urls),
     path('api/', include('games.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
